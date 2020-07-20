8697809b401fea318cb6a9cee7f8b882dfc084c6
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-07-19 18:47:45 -0600

    finishes binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -11,6 +11,22 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        middle = (high + low) // 2
+        if arr[middle] == target:
+            return middle
+        elif arr[high] == target:
+            return high
+        elif arr[low] == target:
+            return low
+        elif target < arr[middle]:
+            high = middle - 1
+        elif target > arr[middle]:
+            low = middle + 1
+
+    return -1
 
 
 
